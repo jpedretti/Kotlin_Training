5bab5c23a600d8ba73d1e84a19585996d0312af1
Ronie Athaydes <ronie@ciandt.com>
2018-02-20 13:34:47 -0300

    Cria propriedade visible nas views para uso no xml

--- app/src/main/java/com/example/jpedretti/kotlintraining/BindingConverters.kt
+++ app/src/main/java/com/example/jpedretti/kotlintraining/BindingConverters.kt
@@ -1,18 +1,17 @@
 package com.example.jpedretti.kotlintraining
 
-import android.view.View.*
+import android.databinding.BindingAdapter
+import android.view.View
+import com.example.jpedretti.kotlintraining.extensions.visible
 
 class BindingConverters {
+
     companion object {
 
         @JvmStatic
-        fun boolToVisibility(boolean: Boolean?) =
-                if (boolean == null) GONE else boolToVisibility(boolean)
-
-        @JvmStatic
-        fun boolToVisibilityNegated(boolean: Boolean?) = boolToVisibility(boolean?.not())
-
-        @JvmStatic
-        private fun boolToVisibility(boolean: Boolean) = if (boolean) VISIBLE else INVISIBLE
+        @BindingAdapter("visible")
+        fun View.setVisible(visible: Boolean) {
+            this.visible = visible
+        }
     }
 }
---
+++ app/src/main/java/com/example/jpedretti/kotlintraining/extensions/ViewExtensions.kt
@@ -0,0 +1,18 @@
+package com.example.jpedretti.kotlintraining.extensions
+
+import android.view.View
+import android.view.View.*
+
+var View.visible: Boolean?
+    get() = when (visibility) {
+        VISIBLE -> true
+        INVISIBLE -> false
+        else -> null
+    }
+    set(value) {
+        visibility = when (value) {
+            true -> VISIBLE
+            false -> INVISIBLE
+            null -> GONE
+        }
+    }
